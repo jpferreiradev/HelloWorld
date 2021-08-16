e6327c6eb63e2eb5cef100bdda89a0771991934e
JP Ferreira <jotepauder01@gmail.com>
2021-08-16 17:18:54 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+// Esse módulo serve para que o c# seja capaz de manipular coisas do SO usúario
+using System.Collections.Generic;
+// Faz parte do System
+using System.Linq;
+// Trabalhar com uma quantidade muito grande de dados
+using System.Text;
+// Permite que você manipule o texto
+using System.Threading.Tasks;
+// Serve para criar um programa que roda em vários núcleos do processador ( Multitread) 
+using System.IO;
+// Serve para você manipular arquivos do seu computador
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Vamos caraio");
+            Console.WriteLine("Quero ver agora!");
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
